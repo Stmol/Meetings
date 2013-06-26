4c86feff31c6cac14044c29ddf5190bb860fa844
Yury Stmol <yury@stmol.me>
2013-06-27 01:43:27 +0400

    Add form for Meeting model

---
+++ src/Stmol/HuddleBundle/Form/MeetingType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Stmol\HuddleBundle\Form;
+
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class MeetingType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', 'text')
+            ->add('description', 'textarea')
+            ->add('startDate', 'date')
+            ->add('save', 'submit')
+        ;
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Stmol\HuddleBundle\Entity\Meeting',
+        ));
+    }
+
+    /**
+     * Returns the name of this type.
+     *
+     * @return string The name of this type
+     */
+    public function getName()
+    {
+        return 'meeting';
+    }
+}
